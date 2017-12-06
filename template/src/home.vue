7c254da4ed0a70816f64f3e95ef3e80cf19afbec
Stefano Somenzi <ssomenzi@gmail.com>
2017-12-06 10:23:50 +0100

    home no pug

--- template/src/home.vue
+++ template/src/home.vue
@@ -1,16 +1,23 @@
-<template lang="pug">
-  div#app
-    f7-views(navbar-through='')
-      f7-view(main='', url='/', :dynamic-navbar='true')
-        f7-navbar
-          f7-nav-left
-          f7-nav-center(sliding='') Home
-          f7-nav-right
-        f7-pages#pages
-          f7-page.navbar-fixed
-            f7-list
-              f7-list-item(title='To page 1' link='/page1/')
-              f7-list-item(title='To page 2' link='/page2/')
+<template>
+    <div id="app">
+        <f7-views navbar-through=''>
+            <f7-view main='' url='/' :dynamic-navbar='true'>
+                <f7-navbar>
+                    <f7-nav-left></f7-nav-left>
+                    <f7-nav-center sliding=''>Home</f7-nav-center>
+                    <f7-nav-right></f7-nav-right>
+                </f7-navbar>
+                <f7-pages id="pages">
+                    <f7-page class="navbar-fixed">
+                        <f7-list>
+                            <f7-list-item title='To page 1' link='/page1/'></f7-list-item>
+                            <f7-list-item title='To page 2' link='/page2/'></f7-list-item>
+                        </f7-list>
+                    </f7-page>
+                </f7-pages>
+            </f7-view>
+        </f7-views>
+    </div>
 </template>
 
 <script>
@@ -24,6 +31,6 @@ export default {
 }
 </script>
 
-<style lang="sass?indentedSyntax">
+<style>
   /* Add your styles here */
 </style>
